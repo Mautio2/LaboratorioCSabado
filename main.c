61b2b22adb1cde1f99c10a3618b8a92d20936531
Mauricio Ceballos <mauricioceballos97@gmail.com>
2019-08-25 22:13:18 -0500

    Calculo1

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+*Autor: Jesus Mauricio Guajardo Ceballos
+*Fecha: 25/08/2019
+*grupo:008
+*Horario: Sabado 9am-11am
+*Matricula:1661852
+*/
 
 int main(int argc, char *argv[]) {
-	//Este comentario es para ideticar el branch Practica3
-	return 0;
+    float x,Calculo1;
+    printf("\nDame el valor de x:");
+    scanf("%f", &x);
+    Calculo1=((x-1)/4)-((x-5)/36);
+    printf("El resultado de el primer calculo es; %.2f\n", Calculo1);
+return 0;a
 }
