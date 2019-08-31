81c221ff35f39033bd117473c2ab99f363b5aeda
Max <maaxx.360@gmail.com>
2019-08-31 16:33:00 -0500

    {

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[]) {
 	//***
-	// Act1	
+	// Act1
 	int a=0, b=0;
 	printf("Programa que determinar el mayor de dos numeros");
 	printf("\nDame el primer numero: ");
@@ -22,12 +22,13 @@ int main(int argc, char *argv[]) {
 		  printf("\n %d es mayor a %d", a,b);}
 	printf("\n\n");
 	// Act2
+	printf("\n----------------------------------------------\n");
 	a=5; b=3;
 	int c=-12;
 	printf("Programa que declare 3 variables de tipo entero y determine si se cumplen las condiciones");
 	//a)
 	printf("\n++++++++++++++++++++++++");
-	printf("\na)\n");
+	printf("\na) ");
 	if (a>3){
 		printf("a es mayor a 3");
 	}
@@ -35,84 +36,183 @@ int main(int argc, char *argv[]) {
 		printf("a es menor o igual a 3");
 	}
 	//b)
-	printf("\n++++++++++++++++++++++++");
-	printf("\nb)\n");
+	printf("\nb) ");
 	if (a>c){
 	    printf("a es mayor a c");}
 	else{
-		printf("c es menor o igual a c ");
+		printf("a es menor o igual a c ");
 	}
 	//c)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\nc)\n");
+	printf("\nc) ");
 	if (b!=c){
-		printf("\nb es diferente de c");
+		printf("b es diferente de c");
 	}
 	else{
-		printf("\nb es igual a c");
+		printf("b es igual a c");
 	}
 	//d)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\nd)\n");
+	printf("\nd) ");
 	if (a==3){
-		printf("\na es igual a 3");
+		printf("a es igual a 3");
 	}
 	else{
-		printf("\na es diferente a 3");
+		printf("a es diferente a 3");
+	}
+	//e)
+	printf("\ne) ");
+	if (a*b==15){
+		printf("El producto entre a y b es 15");
+	}
+	else{
+		printf("El producto entre a y b no es 15");
 	}
 	//f)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\nf)\n");
+	printf("\nf) ");
 	if ((c/b)>a){
-		printf("\nEl cociente de c entre b es menor a a");
+		printf("El cociente de c entre b es mayor a a");
 	}
 	else{
-		printf("\nEl cociente de c entre b no es menor a a");
+		printf("El cociente de c entre b es menor a a");
 	}//g)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\ng)\n");
-	if ((c/b)==-10)){
-		printf("\nEl cociente entre c y b es igual a -10");
+	printf("\ng) ");
+	if ((c/b)==-10){
+		printf("El cociente entre c y b es igual a -10");
 	}
 	else{
-		printf("\nEl cociente entre c y b es diferente a -10");
+		printf("El cociente entre c y b es diferente a -10");
 	}
 	//h)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\nh)\n");
+	printf("\nh) ");
 	if (a+b+c==5){
-		printf("\nLa suma de a,b y c es igual a 5");
+		printf("La suma de a,b y c es igual a 5");
 	}
 	else{
-		printf("\naLa suma de a,b y c es diferente a 5");
+		printf("La suma de a,b y c es diferente a 5");
 	}
 	//i)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\ni)\n");
-	if ((a+b==8)&&){
-		printf("\na es igual a 3");
+	printf("\ni) ");
+	if (((a+b)==8)&&((a-b)==2)){
+		printf("La suma de a y b es igual a 8 y la resta a y b es igual a 2");
 	}
 	else{
-		printf("\na es diferente a 3");
+		printf("La suma de a y b no es 8 o la resta de a menos b es 2");
 	}
 	//j)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\nj)\n");
-	if (a==3){
-		printf("\na es igual a 3");
+	printf("\nj) ");
+	if ((a+b)==8||(a-b==6)){
+		printf("La suma de a y b es igual a 8 o la resta de a menos b es 6");
 	}
 	else{
-		printf("\na es diferente a 3");
+		printf("La suma de a y b no es 8 y la resta de a menos b no es 6");
 	}
 	//o)
-	printf("\n+++++++++++++++++++++++++");
-	printf("\no)\n");
-	if (a==3){
-		printf("\na es igual a 3");
+	printf("\no) ");
+	if (a>3&&b>3&&c<3){
+		printf("a es mayor a 3, b es mayor a 3 y c es menor que 3");
 	}
 	else{
-		printf("\na es diferente a 3");
+		printf("a es menor o igual a 3, b es menor o igual a 3 o c es mayor igual a 3 ");
 	}
-	system("pause");	
+	printf("\n++++++++++++++++++++++++");
+	//***
+	//ACT EXTRA
+	printf("\n\n----------------------------------------------");
+	printf("\nPrograma que declare 3 variables de tipo entero y determine si se cumplen las condiciones");
+	printf("\nPrimero ingrese el valor de las 3 variables: ");
+	printf("\nIngrese el valor de la variable a: ");
+	scanf("%d", &a);
+	printf("Ingrese el valor de la variable b: ");
+	scanf("%d", &b);
+	printf("Ingrese el valor de la variable c: ");
+	scanf("%d", &c);
+	//a)
+	printf("++++++++++++++++++++++++");
+	printf("\na) ");
+	if (a>3){
+		printf("a es mayor a 3");
+	}
+	else {
+		printf("a es menor o igual a 3");
+	}
+	//b)
+	printf("\nb) ");
+	if (a>c){
+			printf("a es mayor a c");}
+	else{
+		printf("a es menor o igual a c ");
+	}
+	//c)
+	printf("\nc) ");
+	if (b!=c){
+		printf("b es diferente de c");
+	}
+	else{
+		printf("b es igual a c");
+	}
+	//d)
+	printf("\nd) ");
+	if (a==3){
+		printf("a es igual a 3");
+	}
+	else{
+		printf("a es diferente a 3");
+	}
+	//e)
+	printf("\ne) ");
+	if (a*b==15){
+		printf("El producto entre a y b es 15");
+	}
+	else{
+		printf("El producto entre a y b no es 15");
+	}
+	//f)
+	printf("\nf) ");
+	if ((c/b)>a){
+		printf("El cociente de c entre b es mayor a a");
+	}
+	else{
+		printf("El cociente de c entre b es menor a a");
+	}//g)
+	printf("\ng) ");
+	if ((c/b)==-10){
+		printf("El cociente entre c y b es igual a -10");
+	}
+	else{
+		printf("El cociente entre c y b es diferente a -10");
+	}
+	//h)
+	printf("\nh) ");
+	if (a+b+c==5){
+		printf("La suma de a,b y c es igual a 5");
+	}
+	else{
+		printf("La suma de a,b y c es diferente a 5");
+	}
+	//i)
+	printf("\ni) ");
+	if (((a+b)==8)&&((a-b)==2)){
+		printf("La suma de a y b es igual a 8 y la resta a y b es igual a 2");
+	}
+	else{
+		printf("La suma de a y b no es 8 o la resta de a menos b es diferente 2");
+	}
+	//j)
+	printf("\nj) ");
+	if ((a+b)==8||(a-b==6)){
+		printf("La suma de a y b es igual a 8 o la resta de a menos b es 6");
+	}
+	else{
+		printf("La suma de a y b no es 8 y la resta de a menos b no es 6");
+	}
+	//o)
+	printf("\no) ");
+	if (a>3&&b>3&&c<3){
+		printf("a es mayor a 3, b es mayor a 3 y c es menor que 3");
+	}
+	else{
+		printf("a es menor o igual a 3, b es menor o igual a 3 o c es mayor o igual a 3 ");
+	}
+	printf("\n++++++++++++++++++++++++\n\n");
+	system("pause");
 	return 0;
 }
