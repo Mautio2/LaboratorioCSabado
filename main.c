dba7af820ef9aa56a8014754a3277d4147a337f0
Raymundo Ruiz <raymundor22@gmail.com>
2019-09-14 10:46:42 -0500

    Practica6, Act1

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* *Autor: Jesu Mauricio Guajardo Ceballos
+   *Fecha:14/09/2019
+   *Grupo:008
+   *Horario: Sabados 9am-11am
+   *Matricula:1661852
+   */
+  
+   int main ()
+   /*ACTIVIDAD 1*/
+{  int  x=0, r=0, SUM=0;
+   printf("Introduzca un numero entero: ");
+   scanf("%d", &x);
+   SUM=x;
+   printf("Desea introducir otro numero entero SI=1 NO=0\n");
+   scanf("%d", &r);
+   while(r==1){
+   printf("Deme el numero:");
+   scanf("%d", &x);
+   SUM=x+SUM;
+   printf("Desea introducir otro numero enteroSI=1 NO=0\n");
+   scanf("%d", &r);
+}
+   printf("La suma de los numeros es %d", SUM);
+   
+   /*ACTIVIDAD 2*/
+   
+   
 
-int main(int argc, char *argv[]) {
-	return 0;
+return 0;
+
 }
