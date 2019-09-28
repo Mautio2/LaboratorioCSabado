0c89b78e185a6dcd60fbb976ff47c0e99a9036c6
Chelsea Olivares <cm.olivares@hotmail.com>
2019-09-28 10:16:35 -0500

    Practica8

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* *Autor:Jesus Mauricio Guajardo Ceballos
+   *Fecha: 28/09/2019
+   *grupo:008
+   *Horario: Sabados 9am-11am
+   *Matricula:1661852
+    */
 
-int main(int argc, char *argv[]) {
-	return 0;
+int main()
+/*ACTIVIDAD1*/
+ { int numeroDeCoches[24]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}; /*determino el arrelo unidimensional con 24 numeros enteros */
+   int R=2;                                                       /*Declaro un entero R, para determinar las horas que representara el indice de mi arreglo*/
+   printf("Programa que  forme un arreglo de 24 numeros que muestre la cantidad y deterimine su hora\n\n");
+   numeroDeCoches[R]=57;                                          /*Determino el valor de la casilla R*/
+   printf("En la hora %d hay %d coches\n", R, numeroDeCoches[R]); /*Se imprime el numero de coches a la hora R*/
+   R=21;                                                          /*Se cambia el valor de R a 21*/
+   numeroDeCoches[R]=57;                                          /*Determino el valor de la casilla R*/
+   printf("En la hora %d hay %d coches\n", R, numeroDeCoches[R]); /*Se imprime el numero de coches a la hora R*/
+   
+   
+   /*ACTIVIDAD2*/
+   int A=8, B=4;                                                 /*Declaro dos variables A y B , y las igualo a 8 y 4 respectivamente*/
+   printf("\nPrograma basado en el anterior que muestre la hora como el cociente de dos numeros\n\n");
+   R=A/B;                                                        /*se iguala la R al cociente entre A y B*/
+   numeroDeCoches[R]=57;                                         /*Determino el valor de la casilla R*/
+   printf("En la hora %d hay %d coches\n", R, numeroDeCoches[R]);/*Se imprime el numero de coches a la hora R*/
+   
+   
+	return 0; /*fin del programa*/
 }
+
