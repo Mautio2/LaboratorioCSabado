a3b6f316542f0b05e76a047a5c43748e90f0b097
Aula403-22 <mauricioceballos97@gmail.com>
2019-10-12 10:57:57 -0500

    Practica10

--- main.c
+++ main.c
@@ -1,8 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* *Autor:Jesus Mauricio Guajardo Ceballos
+   *Fecha: 12/10/2019
+   *grupo:008
+   *Horario: Sabados 9am-11am
+   *Matricula:1661852
+    */
 
-int main(int argc, char *argv[]) {
+int main() {
+	
+	/*Activdad 1*/
+	
+	char cadena1[6];
+	printf("Programa que identifique si una cadena de caracteres es un palindromo\n");
+	printf("Introduzca el primer caracter:\n");
+	scanf("%c", &cadena1[0]);
+	printf("\nIntroduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena1[1]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena1[2]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena1[3]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena1[4]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena1[5]);
+	if (cadena1[0]==cadena1[5]&&cadena1[1]==cadena1[4]&&cadena1[2]==cadena1[3]){
+	
+		
+			printf("La cadena de caracteres %c%c%c%c%c%c es un palindromo \n" , cadena1[0], cadena1[1], cadena1[2], cadena1[3], cadena1[4], cadena1[5]);
+		}
+		else {
+			printf("La cadena de caracteres %c%c%c%c%c%c no es un palindromo \n" , cadena1[0], cadena1[1], cadena1[2], cadena1[3], cadena1[4], cadena1[5]);
+		
+		} 
+	
+		
+	
+	/*Actividad 2*/
+	
+	printf(" Programa que calcule cuantas letras  se repiten en un arreglo de 20 caracteres");
+	char cadena2[20];
+	int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0, o=0, p=0, q=0, r=0, s=0, t=0, u=0, v=0, w=0, x=0, y=0, z=0;
+	printf("Introduzca el primer caracter:\n");
+	scanf("%c", &cadena2[0]);
+	printf("\nIntroduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[1]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[2]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[3]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[4]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[5]);
+	printf("Introduzca el primer caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[6]);
+	printf("\nIntroduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[7);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[8]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[9]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[10]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[10);
+	fflush(stdin);
+	printf("Introduzca el primer caracter:\n");
+	scanf("%c", &cadena2[11]);
+	printf("\nIntroduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[12]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[13]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[14]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[15]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[16]);
+	fflush(stdin);
+	printf("Introduzca el primer caracter:\n");
+	scanf("%c", &cadena2[17]);
+	printf("\nIntroduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[18]);
+	printf("Introduzca el siguiente caracter:\n");
+	fflush(stdin);
+	scanf("%c", &cadena2[19]);
+	
+
+
+	
+	
+	
+
+	
+	
 	return 0;
 }
