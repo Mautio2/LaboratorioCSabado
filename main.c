7a6207e257bb7a1ffd3fc90f3592defb6ad089e0
Aula403-21 <mauricioceballos97@gmail.com>
2019-10-26 10:59:40 -0500

    Practica12

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* *Autor:Jesus Mauricio Guajardo Ceballos
+   *Fecha: 26/10/2019
+   *grupo:008
+   *Horario: Sabados 9am-11am
+   *Matricula:1661852
+    */
+int primo_es(int n);
+int pow(int n, int up);
 
-int main(int argc, char *argv[]) {
-	return 0;
-}
+int main() {
+	
+	/*Activdad 1*/
+	
+	int primo=0, i=0, sum=0; /*Se establece la variable entera para el numero primo*/
+	printf("Programa que dado un numero entero mayor que cero identifique si es un numero primo\n");
+	printf("Introduzca el numero\n"); /*Se introduce el numero*/
+	scanf("%d", &primo);
+	for(i=1;i<=primo;i++){
+	
+	if ((primo%i)==0){
+		sum=sum+i;
+		}
+		}
+		
+		
+		if (sum==primo+1){
+			printf("El numero introducido es un numero primo\n\n");
+		}
+		return 0;
+		
+		/*Actividad2*/
+		
+	
+	}
+	
+	
+	
