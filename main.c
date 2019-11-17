12bda6091366572f12664b7ab71ffcef34fedcc5
Mauricio Ceballos <mauricioceballos@MacBook-Air-de-Mauricio.local>
2019-11-16 23:35:29 -0600

    Actividad1 actividad2

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 //Actividad1
-char cuantum[20];
-FILE *fp;
-	fp = fopen ( "goto1.txt", "r" );
-FILE *fp1;
-	fp1 = fopen ( "archivo_alrevez.txt", "w" ); 
+	char cuantum[100];
+	char reversed[100];
+	FILE *fp;
+	fp = fopen("goto1.txt", "r");
+	FILE *fp1;
+	fp1 = fopen("archivo_alrevez.txt", "w");
 	fgets(cuantum,20,fp);
-	
+	int i;
+	for(i=0;i<strlen(cuantum);i++){
+		reversed[i] = cuantum[strlen(cuantum)-1-i];
+	}
+	fprintf(fp1, "%s", reversed);
+
+
 	//Escribir en el archivo
-	fprintf(fp1, "%s", cuantum);
+	FILE *fp2;
+	FILE *fp3;
+	int a, b;
+	fp2= fopen ("goto2.txt", "r");
+	fp3= fopen ("archivo_de_salida.txt", "w");
+	fscanf(fp2, "%d %d", &a, &b);
+	fprintf(fp3, "La suma %d y %d es %d",a,b, a+b);
 	return 0;
-	
+
 }
-
-
